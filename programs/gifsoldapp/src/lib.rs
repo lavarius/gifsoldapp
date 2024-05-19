d469dc0440feea44c5fe6312732c4c4ba806ebc6
Mark Bartolo <hello@mark-bartolo.com>
2024-05-18 18:53:51 -0700

    passing test with signature fail

--- programs/gifsoldapp/src/lib.rs
+++ programs/gifsoldapp/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("5oVbMwkAJ8ffmkmwNhjckShxxPwDcRvdr2jWgrLZxK8F");
 
@@ -6,10 +7,23 @@ declare_id!("5oVbMwkAJ8ffmkmwNhjckShxxPwDcRvdr2jWgrLZxK8F");
 pub mod gifsoldapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs = 0;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer=user, space=8 + 8)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
--- tests/gifsoldapp.js
+++ tests/gifsoldapp.js
@@ -1,13 +1,40 @@
-const anchor = require("@coral-xyz/anchor");
+// Import statements
+import assert from "assert";
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+const { SystemProgram } = anchor.web3;
+// const anchor = require("@coral-xyz/anchor");
 
 describe("gifsoldapp", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
+  const program = anchor.workspace.Gifsoldapp;
+  let baseAccount;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Gifsoldapp;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Starts stuff off, confirms and gets the transaction", async () => {
+    baseAccount = anchor.web3.Keypair.generate();
+    const startStuffOffTx = await program.methods.startStuffOff().accounts({
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    }).signers([baseAccount]).rpc();
+
+    await program.provider.connection.confirmTransaction(startStuffOffTx);
+    console.log("Confirmed start_stuff_off transaction.");
+    console.log('txSignature:', startStuffOffTx.signature);
+
+    try {
+      const signatureStatuses = await program.provider.connection.getSignatureStatuses([startStuffOffTx.signature]);
+      console.log("Signature Statuses:", signatureStatuses);
+    } catch (error) {
+      console.error("Failed to get signature statuses:", error);
+    }
+  });
+
+  it("Fetches the total GIF count", async () => {
+    // const baseAccount = anchor.web3.Keypair.generate();
+    let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('GIF Count', account.totalGifs.toString());
   });
 });
